156d2ad36a7f62de99965c1a3c19ccaa4b616076
Sergio Oscar Gtz B <sergio.oscar.gb@gmail.com>
2017-01-22 11:45:50 -0600

    creando un modelo de datos

--- app/app.component.js
+++ app/app.component.js
@@ -1,34 +1,47 @@
-System.register(["angular2/core"], function (exports_1, context_1) {
+System.register(['angular2/core', './model/pelicula'], function(exports_1, context_1) {
     "use strict";
+    var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
         else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
         return c > 3 && r && Object.defineProperty(target, key, r), r;
     };
-    var __moduleName = context_1 && context_1.id;
-    var core_1, AppComponent;
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1, pelicula_1;
+    var AppComponent;
     return {
-        setters: [
+        setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
-            }
-        ],
-        execute: function () {
-            // Clase del componente donde iran los datos y funcionalidades
+            },
+            function (pelicula_1_1) {
+                pelicula_1 = pelicula_1_1;
+            }],
+        execute: function() {
+            // Decorador component, indicamos en que etiqueta se va a cargar la plantilla
             AppComponent = (function () {
                 function AppComponent() {
+                    this.titulo = "Películas con Angular2";
+                    this.pelicula = new pelicula_1.Pelicula(1, "Batman vs Superman", "Zack Snider", 2016);
+                    this.debug();
                 }
+                AppComponent.prototype.debug = function () {
+                    console.log(this.pelicula);
+                };
+                AppComponent = __decorate([
+                    core_1.Component({
+                        selector: 'my-app',
+                        templateUrl: 'app/view/peliculas.html'
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], AppComponent);
                 return AppComponent;
             }());
-            AppComponent = __decorate([
-                core_1.Component({
-                    selector: 'my-app',
-                    template: '<h1>Hola mundo con Angular 2 !! victorroblesweb.es</h1>'
-                })
-            ], AppComponent);
             exports_1("AppComponent", AppComponent);
         }
-    };
+    }
 });
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,27 @@
 // Importar el núcleo de Angular
 import {Component} from 'angular2/core';
+import {Pelicula} from './model/pelicula';
 
 // Decorador component, indicamos en que etiqueta se va a cargar la plantilla
 @Component({
     selector: 'my-app',
-    template: '<h1>Hola mundo con Angular 2 !! victorroblesweb.es</h1>'
+    templateUrl: 'app/view/peliculas.html'
 })
 
 // Clase del componente donde iran los datos y funcionalidades
 export class AppComponent {
+  public titulo:string;
+  public pelicula:Pelicula;
 
+  constructor() {
+    this.titulo = "Películas con Angular2";
+
+    this.pelicula = new Pelicula(1, "Batman vs Superman", "Zack Snider", 2016);
+
+    this.debug();
+  }
+
+  debug() {
+    console.log(this.pelicula);
+  }
 }
